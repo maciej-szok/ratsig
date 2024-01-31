ab5026cc506d48adc4c9b2512921aeaae089e813
Maciej <maciej.szok12@gmail.com>
2024-01-31 16:57:43 +0100

    feat: add missing migrations

---
+++ backend/app/alembic/versions/d082bbc0b24f_first_migrations.py
@@ -0,0 +1,24 @@
+"""first migrations
+
+Revision ID: d082bbc0b24f
+Revises: d4867f3a4c0a
+Create Date: 2024-01-23 22:54:20.102651
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd082bbc0b24f'
+down_revision = 'd4867f3a4c0a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
